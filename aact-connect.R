c88c40d9eef6cd19ce9a3224a711a6f8c328e365
Clare Gibson <clarelgibson@gmail.com>
2021-12-05 22:36:22 +0000

    Adds data

--- aact-connect.R
+++ aact-connect.R
@@ -28,5 +28,25 @@ q1 <-
 df_q1 <- dbGetQuery(con, q1)
 
 q2 <- 
-  "SELECT DISTINCT study_type FROM studies"
+  "SELECT DISTINCT enrollment_type FROM studies"
 df_q2 <- dbGetQuery(con, q2)
+
+q3 <- 
+  "SELECT DISTINCT mesh_type FROM browse_conditions"
+df_q3 <- dbGetQuery(con, q3)
+
+q4 <- 
+  "SELECT * FROM baseline_measurements LIMIT 100"
+df_q4 <- dbGetQuery(con, q4)
+
+q5 <- 
+  "SELECT DISTINCT title FROM baseline_measurements"
+df_q5 <- dbGetQuery(con, q5)
+
+q6 <- 
+  "SELECT * FROM baseline_measurements WHERE title = 'Race / Ethnicity'"
+df_q6 <- dbGetQuery(con, q6)
+
+q7 <- 
+  "SELECT DISTINCT gender FROM eligibilities"
+df_q7 <- dbGetQuery(con, q7)
